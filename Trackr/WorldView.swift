050bed8a326ef23881e8322d2d566c1246d5e38c
AbGhost-cyber <xplendo@gmail.com>
2022-12-11 18:36:34 +0800

    added mapview

---
+++ Trackr/WorldView.swift
@@ -0,0 +1,29 @@
+//
+//  WorldView.swift
+//  Trackr
+//
+//  Created by dremobaba on 2022/12/11.
+//
+
+import SwiftUI
+import MapKit
+
+struct WorldView: View {
+    //starting region
+    @State var region = MKCoordinateRegion(
+        center:CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+        span: MKCoordinateSpan(
+            latitudeDelta: 40, longitudeDelta: 40
+        )
+    )
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .navigationTitle("Locations")
+    }
+}
+
+struct WorldView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldView()
+    }
+}
